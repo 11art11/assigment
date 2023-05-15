90c6048100ae773ea47ee4469781040484a65c8a
Wija, Artur <artur.wija.ctr@sabre.com>
2023-05-15 15:32:25 +0200

    * add more debugging

--- docker_utils/dockercomposelogs.py
+++ docker_utils/dockercomposelogs.py
@@ -10,10 +10,6 @@ class DockerComposeLogManager:
         since_str = f"--since '{since}'" if since else ""
         os.system(
             f"docker-compose -f {self.compose_file_path} logs -t --no-color {since_str} >> {self.log_file_path}")
-        with open(self.log_file_path, 'r') as file:
-            content = file.readlines()
-            for line in content:
-                print(line)
 
 
 
